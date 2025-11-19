5167607a2d87b2ff839eca553751e95847ea5ec8
KirakZaz <kirka.zaz@gmail.com>
2025-11-20 10:34:14 +1100

    navigation

--- src/components/HeaderNavigation/HeaderNavigation.jsx
+++ src/components/HeaderNavigation/HeaderNavigation.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@mui/material';
@@ -5,37 +6,36 @@ import { Box } from '@mui/system';
 
 import { routes } from '@/shared/types.js';
 
+import { buttons } from './data.js';
+
 const HeaderNavigation = () => {
     const navigate = useNavigate();
+    const [selectedButton, setSelectedButton] = React.useState('home');
 
-    const buttons = [
-        {
-            key: 'home',
-            label: 'Home',
-            onClick: () => {
-                navigate(routes.home);
-            },
-        },
-        {
-            key: 'game',
-            label: 'Game',
-            onClick: () => {
-                navigate(routes.game);
-            },
-        },
-        {
-            key: 'portfolio',
-            label: 'Portfolio',
-            onClick: () => {
-                navigate(routes.portfolio);
-            },
-        },
-    ];
+    React.useLayoutEffect(() => {
+        const path = window.location.pathname;
+
+        const routeKey = Object.keys(routes).find((key) => routes[key] === path);
+
+        if (routeKey) {
+            setSelectedButton(routeKey);
+        }
+    }, []);
+
+    const onButtonClick = (key) => {
+        setSelectedButton(key);
+        navigate(routes[key]);
+    };
 
     return (
-        <Box>
+        <Box sx={{ ml: 2 }}>
             {buttons?.map((button) => (
-                <Button key={button.key} onClick={button.onClick} style={{ marginRight: 8 }}>
+                <Button
+                    key={button.key}
+                    onClick={() => onButtonClick(button.key)}
+                    variant={selectedButton === button.key ? 'outlined' : 'text'}
+                    style={{ marginRight: 8, fontSize: 16, borderBottom: '1px solid', padding: '2px 8px' }}
+                >
                     {button.label}
                 </Button>
             ))}
---
+++ src/components/HeaderNavigation/data.js
@@ -0,0 +1,19 @@
+import { routes } from '@/shared/types.js';
+
+export const buttons = [
+    {
+        key: 'home',
+        label: 'Home',
+        route: routes.home,
+    },
+    {
+        key: 'game',
+        label: 'Game',
+        route: routes.game,
+    },
+    {
+        key: 'portfolio',
+        label: 'Portfolio',
+        route: routes.portfolio,
+    },
+];
